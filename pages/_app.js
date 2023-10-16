facc48fbedeeddcf2a37e2d33eba58f89229c4ad
Salmonbear <si_bruce@hotmail.com>
2023-10-16 20:23:23 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,17 @@ export default function CustomApp({ Component, pageProps }) {
   return (
      <>
       <Component {...pageProps} />
-      <script defer data-domain="weddingspeechpro.io" src="https://plausible.io/js/script.js"></script>
-     </>
+      <script defer data-domain="weddingspeechpro.io" src="https://plausible.io/js/script.js"></script></>
+    <!-- Hotjar Tracking Code for Wedding Speech Pro -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3696670,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
   );
 }
