227b38d32fb28a411f7afd7ebe27ab9242a5bc37
Salmonbear <si_bruce@hotmail.com>
2023-10-16 19:28:59 +0100

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+<>
+ <Component {...pagesProps} />
+ <Script
+      id="beam-analytics"
+      strategy="afterInteractive"
+      src="https://beamanalytics.b-cdn.net/beam.min.js"
+      data-token="f76cd8af-c1f3-4945-8694-b3733e439047"
+      async>
+    </Script>
+</>
