4a16dc6a1b0b674a3238e4e01a44b7efea3c04d5
Salmonbear <si_bruce@hotmail.com>
2023-10-25 22:46:21 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ export default function CustomApp({ Component, pageProps }) {
      <>
       <Component {...pageProps} />
       <script defer data-domain="weddingspeechpro.io" src="https://plausible.io/js/script.js"></script>
+      <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="ad2ff61b-ed8d-49c9-bb5e-035c5a76f901";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
      </>
   );
 }
